015807691b8c4a187bcd87b88ff7baeef25abbac
iraliyev <ilkin.aliyev@pashabank.az>
2021-09-27 16:53:27 +0400

    Added queue listener

---
+++ src/main/java/ms/customer/blacklist/queue/QueueListener.java
@@ -0,0 +1,30 @@
+package ms.customer.blacklist.queue;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ms.customer.blacklist.model.dto.BlacklistDto;
+import ms.customer.blacklist.service.BlacklistService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class QueueListener {
+
+    private final ObjectMapper objectMapper;
+    private final BlacklistService blacklistService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.blacklist}")
+    public void receiveMessage(String message) {
+        log.info("ActionLog.receiveMessage.start message: {}", message);
+
+        try {
+            var dto = objectMapper.readValue(message, BlacklistDto.class);
+            blacklistService.addIntoBlacklist(dto);
+        } catch (Exception ex) {
+            log.error("ActionLog.receiveMessage.error: ", ex);
+        }
+    }
+}
